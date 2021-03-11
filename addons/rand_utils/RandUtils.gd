412c7772d43f793f40c1d5d037ec51f1572228a5
Distrikt64 <8573006+JulienVanelian@users.noreply.github.com>
2021-03-11 23:00:21 +0100

    Added 2 functions

--- addons/rand_utils/RandUtils.gd
+++ addons/rand_utils/RandUtils.gd
@@ -1,6 +1,30 @@
 tool
 extends Node
 
+func get_rand_bool() -> bool:
+	randomize()
 
-func _enter_tree():
-	pass
+	return bool(randi() % 2)
+
+func get_rand_from_array(array: Array, num: int = 1, unique: bool = false):
+	assert(num >= 1 and num <= len(array), "RandUtils ERROR: Out of bounds.")
+
+	randomize()
+
+	if len(array) == 1:
+		return array[0]
+	elif num == 1:
+		return array[randi() % len(array)]
+	else:
+		var results = []
+		var counter = num
+
+		while counter > 0:
+			var index = randi() % len(array)
+			results.append(array[index])
+			counter -= 1
+
+			if unique:
+				array.remove(index)
+
+		return results
