40f76d383d54b4d0de05b3a779f042cfdd055ed1
Julien Vanelian <8573006+JulienVanelian@users.noreply.github.com>
2021-03-14 17:57:54 +0100

    Added rand vec2 and 3, added rand color

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # godot-rand-utils
 RNG (Random Number Generator) utilities.
+
+## Limitations
+
+Due to the methods being static, it's not possuble to use them in Threads.
--- addons/rand_utils/RandUtils.gd
+++ addons/rand_utils/RandUtils.gd
@@ -14,6 +14,25 @@ static func get_rand_bool(probability: float = .5) -> bool:
 
 	return bool(randf() < 1 - probability)
 
+## Returns a normalized Vector2
+static func get_rand_vec2():
+	randomize()
+
+	return Vector2(randf(), randf())
+
+## Returns a normalized Vector3
+static func get_rand_vec3():
+	randomize()
+
+	return Vector3(randf(), randf(), randf())
+
+## Returns a Color instance with randomized properties
+static func get_rand_color(hueMin: float = 0, hueMax: float = 1, saturationMin: float = 0, saturationMax: float = 1, valueMin: float = 0, valueMax: float = 1, alphaMin: float = 1, alphaMax: float = 1):
+	randomize()	
+	var opaque = alphaMin == alphaMax
+	
+	return Color.from_hsv(rand_range(hueMin, hueMax), rand_range(saturationMin, saturationMax), rand_range(valueMin, valueMax), 1.0 if opaque else rand_range(alphaMin, alphaMax))
+
 ## Returns one or multiple random elements from an array
 static func get_rand_from_array(array: Array, num: int = 1, unique: bool = false):
 	assert(num >= 1 and num <= len(array), "RandUtils ERROR: Out of bounds.")
